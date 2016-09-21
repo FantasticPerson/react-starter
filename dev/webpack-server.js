67b964de1b8fb9648bb8dde1c10a7ef67a91c816
dandan.wu <1240172019@qq.com>
2016-09-21 22:17:38 +0800

    update

--- dev/webpack-server.js
+++ dev/webpack-server.js
@@ -1,32 +1,16 @@
 'use strict';
 
-// import '../src/macros-init'
 global.__WEB_PACK_SERVER__ = true;
 
 import * as DEV_CONST from './const'
-
 import webpack from 'webpack'
 import WebpackDevServer from 'webpack-dev-server'
-
 import {webpackConfig} from './webpack.config.babel'
 
 let express = require('express');
-
 let config = webpackConfig({
     devServer: true
 });
-
-//process.on('uncaughtException', globalErrHandler);
-//process.on('unhandledRejection', globalErrHandler);
-//
-//function globalErrHandler(err, promise) {
-//  let errorStackTrace = err instanceof Error ? err.stack || err.stacktrace || "" : err.toString();
-//
-//  let errMsg = `p:\n ${!!promise} Err:\n ${errorStackTrace}`;
-//  console.error(`Webpack Server crash ${errMsg}`);
-//}
-
-let devPort = 3999;
 
 let devServer = new WebpackDevServer(webpack(config), {
     contentBase: DEV_CONST.OUTPUT_WEB_DIR,
@@ -39,8 +23,6 @@ let devServer = new WebpackDevServer(webpack(config), {
         '*': {
             changeOrigin:true,
             target: 'http://61.155.85.77:10006',
-            // host:'http://61.155.85.77:10006',
-            // secure: false,
         }
     },
     stats: { colors: true },
@@ -50,23 +32,13 @@ let devServer = new WebpackDevServer(webpack(config), {
     quiet: false,
     noInfo: false,
     index: 'main.html'
-    //staticOptions: {
-    //  maxAge: 0,
-    //  index: 'main.html'
-    //}
-    //historyApiFallback: {
-    //  index: 'main.html'
-    //}
 });
 
 devServer.use(express.static(DEV_CONST.ASSETS_PUBLIC_DIR, {maxAge:0}));
-
-devServer.listen(devPort, '0.0.0.0', (err) => {
+devServer.listen(DEV_CONST.DEV_PORT, '0.0.0.0', (err) => {
     if (err) {
         console.error(err);
     }
-    console.log(`webpack dev server start, http://127.0.0.1:${devPort}`);
+    console.log(`webpack dev server start, http://127.0.0.1:${DEV_CONST.DEV_PORT}`);
 });
-
-//we lost the static assets add  here.
 
--- dev/webpack.config.babel.js
+++ dev/webpack.config.babel.js
@@ -1,23 +1,15 @@
 'use strict';
 
-// import '../src/macros-init'
-import * as DEV_CONST from './const'
 import path from 'path'
-
+import * as DEV_CONST from './const'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import webpack, {
-    DefinePlugin,
-    NoErrorsPlugin,
-    HotModuleReplacementPlugin,
-    ProgressPlugin} from 'webpack';
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
-// import WebpackErrorPlaugin from './webpack/WebpackErrorPlaugin'
+import webpack, {DefinePlugin, NoErrorsPlugin, HotModuleReplacementPlugin,} from 'webpack';
 
-//Dynamic config
-//-------------------------------------
 let buildDate = new Date();
-const BUILD_TIME_STAMP = buildDate.getTime();
-const BUILD_TIME_STR = buildDate.toString();
+let __DEBUG__ = true;
+let BUILD_TIME_STAMP = buildDate.getTime();
+let BUILD_TIME_STR = buildDate.toString();
 
 function vendorsConfigs(options) {
     return {
@@ -34,7 +26,6 @@ function noParseConfig(options) {
     for (let vendorKey in r_vendorsConfigs) {
         noParseConfigResult.push(r_vendorsConfigs[vendorKey]);
     }
-
     return noParseConfigResult;
 }
 
@@ -43,69 +34,14 @@ function headerEmbedJs(options) {
       window.__APP_NAME__ = "dandan.wu";
       window.__VERSION__ = "0.0.1";
       window.__APP_ENV__ = "dev";
-      window.__SWFVERSION__ = "v2";
-      window.__ILEARNING_SWF_VERSION__ = "v2";
-      window.__DEV__ = ${__DEV__};
-      window.__PROD__ = ${__PROD__};
-      window.__DEBUG__ = ${__DEBUG__};
+      window.__DEBUG__ = ${DEV_CONST.APP_DEBUG};
       window.__BUILD_TIME_STAMP__ = ${BUILD_TIME_STAMP};
       window.__BUILD_TIME_STR__ = "${BUILD_TIME_STR}";
       window.__ELECTRON__ = false;
-      if((typeof require !== 'undefined') && require.length === 1) {
-        //__ELECTRON__ is runtime check.
-        window.render_electron = require('electron');
-        if(!!render_electron) {
-          window.__ELECTRON__ = true;
-
-          window.os = require('os');
-
-          window.native_remote = render_electron.remote;
-          window.native_require = native_remote.require;
-          window.render_require = require;
-          window.native_getGlobal = native_remote.getGlobal;
-
-          window.__PLATFORM__ = native_getGlobal('__PLATFORM__');
-          window.__ARCH__ = native_getGlobal('__ARCH__');
-          window.__WIN__ = __PLATFORM__ === 'win32';
-          window.__MAC__ = __PLATFORM__ === 'darwin';
-
-          window.__LAUNCH_TIME__ = native_getGlobal('__LAUNCH_TIME__');
-          window.electron = native_getGlobal('electron');
-          window.electron_app = native_getGlobal('electron_app');
-          window.electron_app_path = native_getGlobal('electron_app_path');
-          window.electron_app_data_path = native_getGlobal('electron_app_data_path');
-          window.electron_app_userData_path = native_getGlobal('electron_app_userData_path');
-          window.electron_app_public_path = native_getGlobal('electron_app_public_path');
-          window.electron_app_subapp_path = native_getGlobal('electron_app_subapp_path');
-          window.electron_app_subapp_rhino_path = native_getGlobal('electron_app_subapp_rhino_path');
-        }
-      } else {
-        window.__LAUNCH_TIME__ = Date.now()
-      }
-
-      //for common js.
-      window.__BROWSER__ = true;
-      window.__NODE__ = !__BROWSER__;
-
-      ${__DEBUG__ ? "console.log('-------------------------------------')" : ""}
-      ${__DEBUG__ ? "console.log('__APP_NAME__:', __APP_NAME__)" : ""}
-      ${__DEBUG__ ? "console.log('__VERSION__:', __VERSION__)" : ""}
-      ${__DEBUG__ ? "console.log('__APP_ENV__:', __APP_ENV__)" : ""}
-      ${__DEBUG__ ? "console.log('__DEV__:', __DEV__)" : ""}
-      ${__DEBUG__ ? "console.log('__DEBUG__:', __DEBUG__)" : ""}
-      ${__DEBUG__ ? "console.log('__LAUNCH_TIME__:', __LAUNCH_TIME__)" : ""}
-      ${__DEBUG__ ? "console.log('__BUILD_TIME_STAMP__:', __BUILD_TIME_STAMP__)" : ""}
-      ${__DEBUG__ ? "console.log('__BUILD_TIME_STR__:', __BUILD_TIME_STR__)" : ""}
-      ${__DEBUG__ ? "console.log('__ELECTRON__:', __ELECTRON__)" : ""}
-      ${__DEBUG__ ? "console.log('-------------------------------------')" : ""}
-      ${__DEBUG__ ? "console.log('')" : ""}
+      window.__LAUNCH_TIME__ = Date.now()
   `;
 }
 
-//console.log(headerEmbedJs);
-let __DEV__ = true;
-let __DEBUG__ = true;
-let __PROD__ = false;
 function entryConfig(options) {
     //let vendorCommonConfig = [DEV_CONST.SRC_WEB_VENDOR_COMMON_FILE];
 
@@ -116,7 +52,7 @@ function entryConfig(options) {
 
     let mainConfig = [
         //...vendorCommonConfig,
-        path.resolve(__dirname,'../web/main.js')
+        DEV_CONST.SRC_WEB_MAIN_FILE
     ];
 
     if (options.devServer) {
@@ -125,29 +61,18 @@ function entryConfig(options) {
             `webpack/hot/only-dev-server`
         ];
 
-        //bootupConfig = [
-        //  ...devServerConfig,
-        //  ...bootupConfig
-        //];
-
         mainConfig = [
             ...devServerConfig,
             ...mainConfig
         ];
     }
 
-    //return {
-    //  [DEV_CONST.WEB_BOOTUP_NAME]: bootupConfig,
-    //  [DEV_CONST.WEB_MAIN_NAME]: mainConfig,
-    //}
-
     return mainConfig;
 }
 
 function outputConfig(options) {
-    console.log(path.resolve(__dirname,'../dist/public'));
     return {
-        path: path.resolve(__dirname,'../dist/public'),
+        path: DEV_CONST.OUTPUT_WEB_DIR,
         publicPath: '',
         filename: `[name]-bundle-[[hash]].js`
     }
@@ -155,18 +80,9 @@ function outputConfig(options) {
 
 function resolveConfig(options) {
     return {
-        root: [
-            path.resolve(__dirname,'../web')
-        ],
+        root: [DEV_CONST.SRC_DIR],
         alias: vendorsConfigs(options),
-        // you can now require('file') instead of require('file.coffee')
-        extensions: [
-            '',
-            '.js',
-            '.css',
-            '.json',
-            '.styl'
-        ]
+        extensions: ['', '.js', '.css', '.json', '.styl']
     }
 }
 
@@ -180,8 +96,7 @@ function nodeConfig(options) {
 
 function htmlWebpackPluginConfig(options) {
     function minifyConfig() {
-        if (__DEBUG__) return false;
-
+        if (DEV_CONST.APP_DEBUG) return false;
         //see https://github.com/kangax/html-minifier#options-quick-reference
         return {
             removeComments: true,
@@ -198,7 +113,7 @@ function htmlWebpackPluginConfig(options) {
         //see default html template engine. https://github.com/blueimp/JavaScript-Templates
         //template: DEV_CONST.SRC_WEB_HTML_BOOTUP_FILE,
         //filename: `./${DEV_CONST.OUTPUT_WEB_HTML_BOOTUP_FILE}`,
-        favicon: path.resolve(__dirname,'../assets/icons/app-16x16.ico'),
+        favicon: DEV_CONST.ASSETS_ICONS_16X16_ICON,
         minify: minifyConfig(),
         headerEmbedJs: headerEmbedJs(options),
         //inject: 'body',
@@ -228,12 +143,8 @@ function externalsConfig(options) {
 //--------------------------------------
 export function webpackConfig(options) {
     options = options || {};
-
     return {
-        //The base directory (absolute path!) for resolving the entry option
-        //But here we have DEV_CONST abs path, so not nessary here.
-        context: path.resolve(__dirname,'../web'),
-        //profile: true,
+        context: DEV_CONST.SRC_DIR,
         bail: true,
         debug: true,
         cache: true,
@@ -241,49 +152,31 @@ export function webpackConfig(options) {
         output: outputConfig(options),
         resolve: resolveConfig(options),
         devtool: 'inline-source-map',
-
-        //only in cli?
-        //watchOptions: __DEV__ ?
-        //{
-        //  aggregateTimeout: 1000,
-        //  poll: 1000
-        //} : false,
-
         target: 'web', //default
-        //target: 'electron', //default
         node: nodeConfig(options),
         externals: externalsConfig(options),
         module: {
             loaders: [
-                //js
                 {
                     test: /\.js[x]?$/,
                     include: path.resolve(__dirname,'../web'),
                     exclude: path.resolve(__dirname,'../node_modules'),
-                    loaders: __DEV__ ? ['babel'] : ['babel']
+                    loaders: __DEBUG__ ? ['babel'] : ['babel']
                 },
-                //css
-                // Extract css files
                 {
                     test: /\.css$/,
                     loader: ExtractTextPlugin.extract(['css'])
                 },
-                // Optionally extract stylus files
-                // or any other compile-to-css language
                 {
                     test: /\.styl$/,
                     loader: ExtractTextPlugin.extract(['css', 'stylus'])
                 },
-                //html
                 {test: /\.html$/, loader: "html"},
                 {test: /\.jade$/, loader: "jade?pretty=true"},
-                //img
                 {
                     test: /\.(png|jpe?g|gif)$/,
                     loader: `url?name=imgs/[name]-[[hash]].[ext]&limit=${DEV_CONST.EMBED_FILE_MAX_SIZE}`
                 },
-
-                //fonts
                 {
                     test: /(\.eot|\.woff2|\.woff|\.ttf|regular\.svg)$/,
                     loader: `file?name=fonts/[name]-[[hash]].[ext]`
@@ -296,26 +189,17 @@ export function webpackConfig(options) {
         },
 
         plugins: [
-
-            __DEV__ ? new HotModuleReplacementPlugin() : null,
-
-            //new ProgressPlugin((percentage, msg) => {
-            //  console.log((percentage * 100) + '%', msg);
-            //  //process.stdout.write(`${(percentage * 100)}'%' ${msg}`);
-            //}),
+            DEV_CONST.APP_DEBUG ? new HotModuleReplacementPlugin() : null,
             new NoErrorsPlugin(),
-
             new webpack.optimize.DedupePlugin(),
 
-            __DEBUG__ ? null : new webpack.optimize.UglifyJsPlugin({
+            DEV_CONST.APP_DEBUG ? null : new webpack.optimize.UglifyJsPlugin({
                 warnings: false,
                 dead_code: true
             }),
 
             new DefinePlugin({
-                    __DEV__: __DEV__,
-                    __PROD__: __PROD__,
-                    __DEBUG__: __DEBUG__
+                    __DEBUG__: DEV_CONST.APP_DEBUG
                 }
             ),
 
@@ -341,9 +225,9 @@ export function webpackConfig(options) {
             //main
             new HtmlWebpackPlugin(Object.assign({
                 //see default html template engine. https://github.com/blueimp/JavaScript-Templates
-                template: path.resolve(__dirname,'../web/templates/main.jade'),
+                template: DEV_CONST.SRC_WEB_HTML_MAIN_FILE,
                 filename: `./${DEV_CONST.OUTPUT_WEB_HTML_MAIN_FILE}`,
-                excludeChunks: path.resolve(__dirname,'../web/templates/bootup.jade')
+                excludeChunks: DEV_CONST.SRC_WEB_HTML_BOOTUP_FILE
             }, htmlWebpackPluginConfig(options))),
 
             //debug
