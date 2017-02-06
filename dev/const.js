39643d7604ceb40c90c72640a8a6fc5bc49650a1
dandan.wu <1240172019@qq.com>
2017-02-06 17:53:33 +0800

    fix error

--- dev/const.js
+++ dev/const.js
@@ -25,8 +25,8 @@ export const SRC_WEB_HTML_STYLES_DIR = path.join(SRC_DIR,'styles');
 export const SRC_WEB_MAIN_FILE = path.join(SRC_DIR,WEB_MAIN_NAME+'.js');
 export const SRC_WEB_BOOTUP_FILE = path.join(SRC_DIR,WEB_BOOTUP_NAME+'.js');
 
-export const SRC_WEB_HTML_BOOTUP_FILE = path.join(SRC_WEB_HTML_TEMPALTES_DIR,WEB_BOOTUP_NAME+'.jade');
-export const SRC_WEB_HTML_MAIN_FILE = path.join(SRC_WEB_HTML_TEMPALTES_DIR,WEB_MAIN_NAME+'.jade');
+export const SRC_WEB_HTML_BOOTUP_FILE = path.join(SRC_WEB_HTML_TEMPALTES_DIR,WEB_BOOTUP_NAME+'.pug');
+export const SRC_WEB_HTML_MAIN_FILE = path.join(SRC_WEB_HTML_TEMPALTES_DIR,WEB_MAIN_NAME+'.pug');
 
 export const ASSETS_DIR = path.join(APP_DIR,'assets');
 export const ASSETS_PUBLIC_DIR = path.join(ASSETS_DIR,'public');
